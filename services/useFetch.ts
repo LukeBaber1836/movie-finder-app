71baa7ae1bd7093951092171641909d51f1ce8ac
Luke Baber <luke.baber1@gmail.com>
2025-03-20 23:13:27 -0500

    finished home view

---
+++ services/useFetch.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+const useFetch = <T>(fetchFunction : () => Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(autoFetch);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            setError(null);
+
+            const result = await fetchFunction();
+            setData(result);
+        } catch (error) {
+            // @ts-ignore
+            setError(error instanceof Error ? error : new Error('An unexpected error occurred'));
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const reset = () => {
+        setData(null);
+        setError(null);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        if (autoFetch) {
+            fetchData();
+        }
+    }, [])
+
+    return { data, loading, error, refetch: fetchData, reset}
+}
+
+export default useFetch;
